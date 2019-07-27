101531cdf36622514b7805b0de5600a41c735bf8
Achyudh Ram <7617287+achyudh@users.noreply.github.com>
2019-07-27 12:35:42 +0530

    Update wordBreak.py

--- python/wordBreak.py
+++ python/wordBreak.py
@@ -2,20 +2,20 @@ class Solution:
     def __init__(self):
         self.memo = dict()
         
-    def isBreakable(self, lo: int, hi: int) -> bool:
-        if lo >= hi:
+    def isBreakable(self, i: int) -> bool:
+        if i >= len(self.s):
             return True
         
-        if (lo, hi) not in self.memo:     
-            for i0 in range(lo, hi):
-                if self.s[lo:i0 + 1] in self.words and self.isBreakable(i0 + 1, hi):
-                    self.memo[(lo, hi)] = True
+        if i not in self.memo:     
+            for i0 in range(i, len(self.s)):
+                if self.s[i:i0 + 1] in self.words and self.isBreakable(i0 + 1):
+                    self.memo[i] = True
                     return True
-            self.memo[(lo, hi)] = False
+            self.memo[i] = False
         
-        return self.memo[(lo, hi)]
+        return self.memo[i]
         
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         self.s = s
         self.words = wordDict
-        return self.isBreakable(0, len(s))
+        return self.isBreakable(0)
